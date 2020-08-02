40437c6291c0ae159fb15b97d57df3db127a638b
Lucas Ambrosi Derrossi <lucas_ambrosi@yahoo.com.br>
2020-08-02 15:52:45 -0300

    Add initial structure

---
+++ src/main/java/com/lucasambrosi/water/lack/notifier/WaterLackNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.lucasambrosi.water.lack.notifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterLackNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterLackNotifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasambrosi/water/lack/notifier/WaterLackNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasambrosi.water.lack.notifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterLackNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
